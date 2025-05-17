946ed49feee22e66c3553aef23b978c8ad7734a6
brianzapana1 <160275021+brianzapana1@users.noreply.github.com>
2025-05-17 11:54:11 -0400

    creacion tres en raya

---
+++ src/main/java/com/grupo/TresEnRaya.java
@@ -0,0 +1,13 @@
+package com.grupo;
+
+public class TresEnRaya {
+    private char[][] tablero = new char[3][3];
+    private char jugadorActual = 'X';
+
+    public TresEnRaya() {
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                tablero[i][j] = ' ';
+    }
+
+}
---
+++ src/test/java/com/grupo/TresEnRayaTest.java
@@ -0,0 +1,10 @@
+package com.grupo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.Test;
+
+public class TresEnRayaTest {
+
+   
+}
