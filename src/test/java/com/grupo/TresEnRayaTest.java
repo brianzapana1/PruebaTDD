785e103e567154f4ec7300d722429eb29c52de88
belu26y <148408404+belu26y@users.noreply.github.com>
2025-05-18 23:44:25 -0400

    Update TresEnRayaTest.java

--- src/test/java/com/grupo/TresEnRayaTest.java
+++ src/test/java/com/grupo/TresEnRayaTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
 
 public class TresEnRayaTest {
-
+//test primera funcion
     @Test
     public void primerTurnoDebeSerX() {
         TresEnRaya juego = new TresEnRaya();
