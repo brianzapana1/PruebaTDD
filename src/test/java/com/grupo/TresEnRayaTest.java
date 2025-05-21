991cd4111395daf6b58254d84c715b4d1d27e0a5
Beymarnexx <160280516+Beymarnexx@users.noreply.github.com>
2025-05-21 02:26:54 -0400

    clase del tres en raya.java

--- src/test/java/com/grupo/TresEnRayaTest.java
+++ src/test/java/com/grupo/TresEnRayaTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
 
+//clase que representa el juego tres en raya
 public class TresEnRayaTest {
 //test primera funcion
     @Test
